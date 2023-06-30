e3b2f4cb16f4d0506443cf2d176ac9235edfa7b3
Guilherme Chaves <guilherme.chaves66@gmail.com>
2023-06-30 12:44:03 -0300

    2 Modificação
    Segunda modificação que implementa o jogo com as respostas.

---
+++ Mastermind Resposta/src/HomeScreen.java
@@ -0,0 +1,97 @@
+public class HomeScreen {
+        public static void defaltHomeScreen() {
+
+                System.out.println(".___  ___." + "      ___" + "          ________." + "___________." + " ________"
+                                + " ._______" + "      .___  ___." + " __" + " .___   ___." + "  _________");
+                System.out.println("|   \\/   |" + "     /   \\" + "        /        |" + "           |" + "|   _____|"
+                                + "|    _  \\" + "     |   \\/   |" + "|  |" + "|   \\ |   |" + " |         \\");
+                System.out.println("|  \\  /  |" + "    /  /\\ \\" + "      |    (----'" + "---|  |----'" + "|  |__"
+                                + "    |   |_)  |" + "    |  \\  /  |" + "|  |" + "|    \\|   |" + " |   .--.   |");
+                System.out.println("|  |\\/|  |" + "   /  /__\\ \\" + "      \\    \\" + "       |  |" + "     |   __|"
+                                + "   |       /" + "     |  |\\/|  |" + "|  |" + "|   . '   |" + " |   |  |   |");
+                System.out.println("|  |  |  |" + "  /  _____  \\ " + " .---)    |" + "      |  |" + "     |  |_____"
+                                + " |   |\\  \\----." + "|  |  |  |" + "|  |" + "|   |\\    |" + " |   '--'   |");
+                System.out.println("|__|  |__|" + " /__/     \\__\\" + " |________/" + "      |__|" + "     |________|"
+                                + "|  _| '._____|" + "|__|  |__|" + "|__|" + "|___| \\___|" + " |_________/");
+
+                System.out.println("                       _" + "  _" + "     _"
+                                + "                                       _");
+                System.out.println("         __        __" + " (_)" + "| |_" + "  | |__" + "        _ __" + "    _   _"
+                                + "   _ __ ___" + "   | |___" + "     ___" + "   _ __" + "   ___");
+                System.out.println("         \\ \\  /\\  / /" + " | |" + "| __|" + " | '_ \\" + "      | '_ \\"
+                                + "  | | | |"
+                                + " | '_ ' _ \\" + "  | '_  \\" + "   / _ \\" + " | '__|" + " / __|");
+                System.out.println(
+                                "          \\ \\/  \\/ /" + "  | |" + "| |_" + "  | | | |" + "     | | | |" + " | |_| |"
+                                                + " | | | | | |" + " | |_)  |" + " |  __/" + " | |" + "    \\__ \\");
+                System.out.println("           \\__/\\__/" + "   |_|" + " \\__|" + " |_| |_|" + "     |_| |_|"
+                                + "  \\__,_|"
+                                + " |_| |_| |_|" + " |_.___/" + "   \\___|" + " |_|" + "    |___/");
+
+                System.out.println();
+                System.out.println();
+                System.out.println(
+                                "      How to play: The computer will generate a 4-digit password, with the digits ranging from 1 to 6.");
+                System.out.println(
+                                "             The objective of the game is to guess this password in a maximum of 10 attempts.");
+                System.out.println(
+                                "          Game Mechanics: After generating the password, the computer will ask the player to try ");
+                System.out.println(
+                                "                     entering a four-digit password as well, returning some information.");
+                System.out.println(
+                                "         The information provided is as follows: How many digits the player guessed correctly and ");
+                System.out.println(
+                                "                        how many digits are correct but in the wrong position.");
+                System.out.println(
+                                "            Based on the computer's responses, you will make your decisions to win the game!");
+
+                System.out.println();
+                System.out.println();
+                System.out.println(
+                                "---------------------------------------------------------------------------------------------------------------------------");
+        }
+
+        public static void winnerEndScreen() {
+                System.out.println(
+                                "   * ' ' *               ___      _____      ___  __  .___   ___. .___   ___.  ________  ._______                     * ' ' *                 ");
+                System.out.println(
+                                " *   \\ /   *             \\  \\    /     \\    /  / |  | |   \\ |   | |   \\ |   | |   _____| |    _  \\                  *   \\ /   *       ");
+                System.out.println(
+                                " * -- O -- * --__         \\  \\  /   _   \\  /  /  |  | |    \\|   | |    \\|   | |  |__     |   |_)  |            __-- * -- O -- *          ");
+                System.out.println(
+                                " *   / \\   *     \\__       \\  \\/   / \\   \\/  /   |  | |   . '   | |   . '   | |   __|    |       /          __/     *   / \\   *        ");
+                System.out.println(
+                                "   * . . *          \\       \\     /   \\     /    |  | |   |\\    | |   |\\    | |  |_____  |   |\\  \\----.    /          * . . *          ");
+                System.out.println(
+                                "                     \\       \\___/     \\___/     |__| |___| \\___| |___| \\___| |________| |  _| '._____|   /                              ");
+
+                System.out.println();
+                System.out.println();
+
+                System.out.println("                       		                 by: Guilherme Chaves");
+                System.out.println("                       		           Universidade Católica do Salvador");
+                System.out.println("                       				     !The End!");
+        }
+
+        public static void gameOverEndScreen() {
+                System.out.println(
+                                " __     ________         ___      .___  ___.  ________        ________  .___.       .__.  ________  ._______        __         ");
+                System.out.println(
+                                "|  |   /  ______|       /   \\     |   \\/   | |   _____|      /   ___  \\  \\  \\      /  /  |   _____| |    _  \\      |  |        ");
+                System.out.println(
+                                "|  |  |  /  _____      /  /\\ \\    |  \\  /  | |  |__         |   :   :  |  \\  \\    /  /   |  |__     |   |_)  |     |  |        ");
+                System.out.println(
+                                "|  |  | |  |___  \\    /  /__\\ \\   |  |\\/|  | |   __|        |   |   |  |   \\  \\  /  /    |   __|    |       /      |  |        ");
+                System.out.println(
+                                "|__|  | |______|  |  /  _____  \\  |  |  |  | |  |_____      |   '___'  |    \\  \\/  /     |  |_____  |   |\\  \\----. |__|        ");
+                System.out.println(
+                                "(__)   \\__________| /__/     \\__\\ |__|  |__| |________|      \\________/      \\____/      |________| |  _| '._____| (__)        ");
+
+                System.out.println();
+                System.out.println();
+
+                System.out.println("                                           by: Guilherme Chaves");
+                System.out.println("                                     Univercidade Católica do Salvador     ");
+                System.out.println("                                               !The End!                   ");
+        }
+}
